0ed050020280400056115fffe063fad70c0cb01f
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2026-03-12 20:20:35 +0200

    feat: implement context creation with session and database integration; add business owner middleware and new router files

--- lib/trpc/context.ts
+++ lib/trpc/context.ts
@@ -1,5 +1,17 @@
-import { inferAsyncReturnType } from "@trpc/server";
 import {CreateNextContextOptions} from "@trpc/server/adapters/next";
 import {getServerSession} from "next-auth";
 import db from "../../drizzle/db";
-import {authConfig} from "@/lib/auth/config"
+import {authConfig} from "@/lib/auth/config";
+
+export async function createContext(opts: CreateNextContextOptions) {
+  const session = await getServerSession(authConfig);
+
+  return{
+    db,
+    session,
+    req: opts.req,
+    res: opts.res
+  };
+}
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
--- lib/trpc/trpc.ts
+++ lib/trpc/trpc.ts
@@ -1,10 +1,10 @@
 import { initTRPC, TRPCError } from '@trpc/server';
-import superjson from 'superjson';
+// import superjson from 'superjson';
 import { ZodError } from 'zod';
 import type { Context } from './context';
 
 const t = initTRPC.context<Context>().create({
-  transformer: superjson,
+  // transformer: superjson,
   errorFormatter({ shape, error }) {
     return {
       ...shape,
@@ -23,7 +23,10 @@ export const publicProcedure = t.procedure;
 // Middleware за проверка дали потребителят е автентикиран
 const isAuthed = t.middleware(({ ctx, next }) => {
   if (!ctx.session?.user) {
-    throw new TRPCError({ code: 'UNAUTHORIZED' });
+    throw new TRPCError({ 
+      code: 'UNAUTHORIZED',
+      message: 'You must be logged in to access this resource.',
+    });
   }
   return next({
     ctx: {
@@ -33,4 +36,40 @@ const isAuthed = t.middleware(({ ctx, next }) => {
   });
 });
 
+const isBusinessOwner = t.middleware(async({ctx, next})=>{
+  if(!ctx.session?.user){
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: 'You must be logged in to access this resource.',
+    });
+  };
+
+  const staff = await ctx.db.query.staff.findFirst({
+    where: (staff, {eq, and})=>
+      and(
+        eq(staff.email, ctx.session.user.email!),
+        eq(staff.role, "owner")
+      ),
+      with:{
+        business: true,
+      }
+  });
+
+  if(!staff){
+    throw new TRPCError({
+      code: 'FORBIDDEN',
+      message: 'You do not have permission to access this resource.',
+    });
+  };
+
+  return next ({
+    ctx: {
+      ...ctx,
+      business: staff.business,
+      staff,
+    }
+  })
+})
+
 export const protectedProcedure = t.procedure.use(isAuthed);
+export const businessProcedure = t.procedure.use(isBusinessOwner);
