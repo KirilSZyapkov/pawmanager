a8decd69485372f813a1b863c27ee4b35608b1c0
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2026-03-15 22:21:31 +0200

    feat: add create and update employee functionalities in staff router

--- lib/trpc/router/staff.ts
+++ lib/trpc/router/staff.ts
@@ -4,7 +4,7 @@ import { router, businessProcedure } from '../trpc';
 import db from '@/drizzle/db';
 import { eq, and } from 'drizzle-orm';
 import { staffSchema } from '@/lib/validators/staff';
-import { appointments } from "@/drizzle/schema";
+import { appointments, staff } from "@/drizzle/schema";
 
 export const staffRouter = router({
 
@@ -37,5 +37,40 @@ export const staffRouter = router({
       };
 
       return employee;
+    }),
+
+  createNewEmployee: businessProcedure
+    .input(staffSchema)
+    .mutation(async ({ ctx, input }) => {
+      const existing = await db.query.staff.findFirst({
+        where: (staff, { eq, and }) =>
+          and(
+            eq(staff.email, input.email),
+            eq(staff.businessId, ctx.business.id)
+          )
+      });
+
+      if (existing) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "Employee already exists"
+        });
+      };
+
+      const [newEmployee] = await db.insert(staff).values(...input, businessId: ctx.business.id).returning();
+
+      return newEmployee;
+    }),
+
+  updateEmployee: businessProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+        data: staffSchema.partial()
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const [updated] = await db.update(staff).set({ ...input.data, updatedAt: new Date() }).where(and(eq(staff.id, input.id), eq(staff.businessId, ctx.business.id))).returning();
+      return updated;
     })
 })
