eb3cef5bab2efbb1d42b87c56887d5e9ce759c5a
Kiril Zyapkov | GOPET TRANS <appdeveloperjs47@gmail.com>
2026-03-19 11:17:23 +0200

    feat: adding listAllAppointments in lAppointments router.

--- lib/trpc/router/appointment.ts
+++ lib/trpc/router/appointment.ts
@@ -3,3 +3,65 @@ import {TRPCError} from "@trpc/server";
 import {router, businessProcedure} from "../trpc";
 import db from "@/drizzle/db";
 import {appointmentSchema} from "@/lib/validators/appointment";
+import { appointments } from "@/drizzle/schema";
+
+export const appointmentRouter = router({
+
+    listAllAppointments: businessProcedure
+    .input(
+        z.object({
+            startDate: z.date().optional(),
+            endDate: z.date().optional(),
+            staffId: z.string().optional(),
+            petId: z.string().optional(),
+            status: z.enum(['pending', 'confirmed', 'completed', 'cancelled']).optional(),
+            limit: z.number().optional(),
+            offset: z.number().min(0).default(0)
+        })
+    )
+    .query(
+        async ({ctx, input})=>{
+            let query = db.query.appointments.findMany({
+                where: (appointments, {and, eq})=> {
+                    const conditions = [eq(appointments.businessId, ctx.business.id)];
+
+                    if(input.startDate && input.endDate){
+                        conditions.push(between(appointments.startTime, input.startDate, input.endDate));
+                    };
+
+                    if(input.staffId){
+                        conditions.push(eq(appointments.staffId, input.staffId));
+                    };
+
+                    if(input.petId){
+                        conditions.push(eq(appointments.petId, input.petId));
+                    };
+
+                    if(input.status){
+                        conditions.push(eq(appointments.status, input.status))
+                    };
+
+                    return and(...conditions);
+                },
+
+                whit: {
+                    pet: {
+                        with: {
+                            client: true,
+                        }
+                    },
+                    service: true,
+                    staff: true,
+                },
+
+                orderBy: (appointments, {desc})=> [desc(appointments.startTime)],
+                limit: input.limit,
+                offser: input.offset,
+            });
+
+            return query;
+        },
+
+        
+    )
+})
