8fc709707dd68d2f7e1081964d5369837381e54f
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2026-03-20 22:14:04 +0200

    feat: adding listAllAppointments and  updateAppointmentRecord.

--- lib/trpc/router/appointment.ts
+++ lib/trpc/router/appointment.ts
@@ -8,142 +8,170 @@ import { and, between, eq } from "drizzle-orm";
 
 export const appointmentRouter = router({
 
-    listAllAppointments: businessProcedure
-        .input(
-            z.object({
-                startDate: z.date().optional(),
-                endDate: z.date().optional(),
-                staffId: z.string().optional(),
-                petId: z.string().optional(),
-                status: z.enum(['pending', 'confirmed', 'completed', 'cancelled']).optional(),
-                limit: z.number().optional(),
-                offset: z.number().min(0).default(0)
-            })
-        )
-        .query(
-            async ({ ctx, input }) => {
-                let query = db.query.appointments.findMany({
-                    where: (appointments, { and, eq }) => {
-                        const conditions = [eq(appointments.businessId, ctx.business.id)];
+  listAllAppointments: businessProcedure
+    .input(
+      z.object({
+        startDate: z.date().optional(),
+        endDate: z.date().optional(),
+        staffId: z.string().optional(),
+        petId: z.string().optional(),
+        status: z.enum(['pending', 'confirmed', 'completed', 'cancelled']).optional(),
+        limit: z.number().optional(),
+        offset: z.number().min(0).default(0)
+      })
+    )
+    .query(
+      async ({ ctx, input }) => {
+        let query = db.query.appointments.findMany({
+          where: (appointments, { and, eq }) => {
+            const conditions = [eq(appointments.businessId, ctx.business.id)];
 
-                        if (input.startDate && input.endDate) {
-                            conditions.push(between(appointments.startTime, input.startDate, input.endDate));
-                        };
+            if (input.startDate && input.endDate) {
+              conditions.push(between(appointments.startTime, input.startDate, input.endDate));
+            };
 
-                        if (input.staffId) {
-                            conditions.push(eq(appointments.staffId, input.staffId));
-                        };
+            if (input.staffId) {
+              conditions.push(eq(appointments.staffId, input.staffId));
+            };
 
-                        if (input.petId) {
-                            conditions.push(eq(appointments.petId, input.petId));
-                        };
+            if (input.petId) {
+              conditions.push(eq(appointments.petId, input.petId));
+            };
 
-                        if (input.status) {
-                            conditions.push(eq(appointments.status, input.status))
-                        };
+            if (input.status) {
+              conditions.push(eq(appointments.status, input.status))
+            };
 
-                        return and(...conditions);
-                    },
+            return and(...conditions);
+          },
 
-                    with: {
-                        pet: {
-                            with: {
-                                client: true,
-                            }
-                        },
-                        service: true,
-                        staff: true,
-                    },
+          with: {
+            pet: {
+              with: {
+                client: true,
+              }
+            },
+            service: true,
+            staff: true,
+          },
 
-                    orderBy: (appointments, { desc }) => [desc(appointments.startTime)],
-                    limit: input.limit,
-                    offset: input.offset,
-                });
+          orderBy: (appointments, { desc }) => [desc(appointments.startTime)],
+          limit: input.limit,
+          offset: input.offset,
+        });
 
-                return query;
-            }),
+        return query;
+      }),
 
-    getAppointmentById: businessProcedure
-        .input(z.object({ id: z.uuid() }))
-        .query(
-            async ({ ctx, input }) => {
-                const appointment = await db.query.appointments.findFirst({
-                    where: (appointments, { and, eq }) =>
-                        and(
-                            eq(appointments.id, input.id),
-                            eq(appointments.businessId, ctx.business.id)
-                        ),
-                    with: {
-                        pet: {
-                            with: {
-                                client: true,
-                            }
-                        },
-                        service: true,
-                        staff: true,
-                        photo: true,
-                        payment: true,
-                    },
-                });
+  getAppointmentById: businessProcedure
+    .input(z.object({ id: z.uuid() }))
+    .query(
+      async ({ ctx, input }) => {
+        const appointment = await db.query.appointments.findFirst({
+          where: (appointments, { and, eq }) =>
+            and(
+              eq(appointments.id, input.id),
+              eq(appointments.businessId, ctx.business.id)
+            ),
+          with: {
+            pet: {
+              with: {
+                client: true,
+              }
+            },
+            service: true,
+            staff: true,
+            photo: true,
+            payment: true,
+          },
+        });
 
-                if (!appointment) {
-                    throw new TRPCError({
-                        code: 'NOT_FOUND',
-                        message: 'Резервацията не е намерена',
-                    })
-                };
+        if (!appointment) {
+          throw new TRPCError({
+            code: 'NOT_FOUND',
+            message: 'Резервацията не е намерена',
+          })
+        };
 
-                return appointment;
-            }),
+        return appointment;
+      }),
 
-    createNewAppointment: businessProcedure
-        .input(appointmentSchema)
-        .mutation(
-            async ({ ctx, input }) => {
-                const conflictting = await db.query.appointments.findFirst({
-                    where: (appointments, { and, eq, between }) =>
-                        and(
-                            eq(appointments.businessId, ctx.business.id),
-                            eq(appointments.staffId, input.staffId),
-                            eq(appointments.status, "confirmed"),
-                            between(
-                                appointments.startTime,
-                                input.startTime,
-                                input.endTime
-                            )
-                        )
-                });
+  createNewAppointment: businessProcedure
+    .input(appointmentSchema)
+    .mutation(
+      async ({ ctx, input }) => {
+        const conflictting = await db.query.appointments.findFirst({
+          where: (appointments, { and, eq, between }) =>
+            and(
+              eq(appointments.businessId, ctx.business.id),
+              eq(appointments.staffId, input.staffId),
+              eq(appointments.status, "confirmed"),
+              between(
+                appointments.startTime,
+                input.startTime,
+                input.endTime
+              )
+            )
+        });
 
-                if (conflictting) {
-                    throw new TRPCError({
-                        code: 'CONFLICT',
-                        message: 'Този час вече е зает',
-                    })
-                };
+        if (conflictting) {
+          throw new TRPCError({
+            code: 'CONFLICT',
+            message: 'Този час вече е зает',
+          })
+        };
 
-                const [newAppointmentRecord] = await db.insert(appointments).values({ ...input, businessId: ctx.business.id }).returning();
+        const [newAppointmentRecord] = await db.insert(appointments).values({ ...input, businessId: ctx.business.id }).returning();
 
-                return newAppointmentRecord;
-            }),
+        return newAppointmentRecord;
+      }),
 
-    updateAppointmentRecord: businessProcedure
-        .input(
-            z.object({
-                id: z.uuid(),
-                data: appointmentSchema.partial(),
-            })
-        )
-        .mutation(
-            async ({ ctx, input }) => {
-                const [updatedAppointmentRecord] = await db.update(appointments).set({ ...input.data, updatedAt: new Date() })
-                    .where(
-                        and(
-                            eq(appointments.id, input.id),
-                            eq(appointments.businessId, ctx.business.id)
-                        )
-                    )
-                    .returning();
+  updateAppointmentRecord: businessProcedure
+    .input(
+      z.object({
+        id: z.uuid(),
+        data: appointmentSchema.partial(),
+      })
+    )
+    .mutation(
+      async ({ ctx, input }) => {
+        const [updatedAppointmentRecord] = await db.update(appointments).set({ ...input.data, updatedAt: new Date() })
+          .where(
+            and(
+              eq(appointments.id, input.id),
+              eq(appointments.businessId, ctx.business.id)
+            )
+          )
+          .returning();
 
-                return updatedAppointmentRecord;
-            })
+        return updatedAppointmentRecord;
+      }),
+
+  changeAppointmentStatus: businessProcedure
+    .input(
+      z.object({
+        id: z.uuid(),
+        status: z.enum(['confirmed', 'completed', 'cancelled', 'no_show']),
+        reason: z.string().optional()
+      })
+    )
+    .mutation(
+      async ({ ctx, input }) => {
+        const rawData = {
+          status: input.status,
+          updatedAt: new Date(),
+        };
+
+        const [updatedAppointmetRecord] = await db.update(appointments)
+          .set(rawData)
+          .where(
+            and(
+              eq(appointments.id, input.id),
+              eq(appointments.businessId, ctx.business.id)
+            )
+          )
+          .returning();
+
+        return updatedAppointmetRecord;
+      })
 })
