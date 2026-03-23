77171d429448df34e0400802d2fdc67ab23d42e5
Kiril Zyapkov | GOPET TRANS <appdeveloperjs47@gmail.com>
2026-03-23 12:56:10 +0200

    feat: adding the main appRouter and the type.

---
+++ lib/trpc/router/index.ts
@@ -0,0 +1,28 @@
+import {router} from "../trpc";
+import { authRouter } from './auth';
+import { businessRouter } from './business';
+import { staffRouter } from './staff';
+import { clientRouter } from './client';
+import { petRouter } from './pet';
+import { serviceRouter } from './service';
+import { appointmentRouter } from './appointment';
+import { photoRouter } from './photo';
+import { paymentRouter } from './payment';
+import { notificationRouter } from './notification';
+import { dashboardRouter } from './dashboard';
+
+export const appRouter = router({
+    auth: authRouter,
+  business: businessRouter,
+  staff: staffRouter,
+  client: clientRouter,
+  pet: petRouter,
+  service: serviceRouter,
+  appointment: appointmentRouter,
+  photo: photoRouter,
+  payment: paymentRouter,
+  notification: notificationRouter,
+  dashboard: dashboardRouter,
+});
+
+export type AppRouter = typeof appRouter;
