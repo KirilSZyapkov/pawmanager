60513290e73f653ca78e296de287b0d9880655d0
Kiril Zyapkov | GOPET TRANS <appdeveloperjs47@gmail.com>
2026-03-23 13:21:23 +0200

    feat: adding API Route Handler

---
+++ app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,14 @@
+import {fetchRequestHandler} from '@trpc/server/adapters/fetch';
+import {NextRequest} from "next/server";
+import {appRouter} from "@/lib/trpc/server";
+import { createContext } from '@/lib/trpc/context';
+
+const handler = (req: NextRequest) =>
+    fetchRequestHandler({
+        endpoint: '/api/trpc',
+        req,
+        router: appRouter,
+        createContext: ()=>createContext(req, res: {} as any), 
+    });
+
+    export {handler as GET, handler as POST};
