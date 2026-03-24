84bf119b7005da21b23daba96dc6fa7f5d0f3c6d
Kiril Zyapkov | GOPET TRANS <appdeveloperjs47@gmail.com>
2026-03-24 10:55:20 +0200

    feat: finish business register function.

--- lib/trpc/router/auth.ts
+++ lib/trpc/router/auth.ts
@@ -2,6 +2,9 @@ import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 import { router, publicProcedure } from "../trpc";
 import db from "@/drizzle/db";
+import { registerBusinessSchema } from "@/lib/validators/business";
+import bcrypt from 'bcryptjs';
+import { businesses, staff } from "@/drizzle/schema";
 
 export const authRouter = router({
   // Get the current user's session
@@ -12,16 +15,58 @@ export const authRouter = router({
   // register a new business
   registerNewBusibess: publicProcedure
     .input(
-      z.object({
-        businessName: z.string().min(2, "Business name must be at least 2 characters long"),
-        email: z.string().email("Invalid email address"),
-        password: z.string().min(6, "Password must be at least 6 characters long"),
-        phoneNumber: z.string().min(10, "Phone number must be at least 10 characters long"),
-      })
+      registerBusinessSchema
     )
     .mutation(async ({ input }) => {
-      // да интегрирам NextAuth
-      return { success: true }
+      const existingBusiness = await db.query.business.findFirst({
+        where: (businesses,{eq, or}) => or(eq(businesses.email, input.email), eq(businesses.phone, input.phone))
+      });
+
+      if (existingBusiness) {
+        throw new TRPCError({
+          code: 'CONFLICT',
+          message: 'Business already exist!',
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(input.password, 10);
+
+      const [business] = await db.insert(businesses).values({
+        name: input.businessName,
+          email: input.email,
+          phone: input.phone,
+          address: input.address,
+          city: input.city,
+          status: 'trial',
+          subscriptionTier: 'basic',
+          subscriptionEndsAt: new Date(Date.now() + 14 * 24 * 60 * 60 * 1000), // 14 дни trial
+          maxStaff: 1,
+          maxClients: 100,
+          smsCredits: 50,
+          settings: {
+            timezone: 'Europe/Sofia',
+            currency: 'BGN',
+            appointmentReminderHours: 24,
+          },
+      }).returning();
+
+      const [owner] = await db.insert(staff).values({
+          businessId: business.id,
+          email: input.email,
+          name: input.ownerName,
+          phone: input.phone,
+          role: 'owner',
+          isActive: true,
+          workingDays: [1, 2, 3, 4, 5], // Понеделник - Петък
+          workingHours: { start: '09:00', end: '18:00' },
+      }).returning();
+
+
+      return { 
+        success: true ,
+        businessId: business.id,
+        message: "Business successfuly created."
+      }
     }),
 
   // login client
